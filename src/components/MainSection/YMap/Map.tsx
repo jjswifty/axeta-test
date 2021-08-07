39af5f9c54c593a3fbb26553d4f6595b1df993b5
jjswift <supercheater2018@yandex.ru>
2021-08-07 08:11:38 +0300

    Map synchronized with global state

--- src/components/MainSection/YMap/Map.tsx
+++ src/components/MainSection/YMap/Map.tsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
 import { Map } from 'react-yandex-maps'
 import { useStoreon } from 'storeon/react'
 import { LocationEvents, LocationState } from '../../../store/location.module'
 
 export const YMap = () => {
 
-    //const [position, setPosition] = useState({
-    //    latitude: null,
-    //    longitude: null
-    //})
+    const { location } = useStoreon<LocationState, LocationEvents>('location')
 
-    const { dispatch, location } = useStoreon<LocationState, LocationEvents>('location')
-    console.log(location, dispatch)
-
-
-    return <Map 
-        //state={{center: [location.latitude, location.longitude], zoom: 10}}  
-        defaultState={{ center: [45.523064, -122.676483], zoom: 10 }}/>
+    return <Map state={{center: [location.latitude, location.longitude], zoom: 10}} />
 }
